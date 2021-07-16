be7e2116b1c4231419fef618b0307b7804ddfef8
Joran Bergfeld <joranbergfeld@hotmail.com>
2021-07-16 14:43:33 +0200

    Fixes casting issue.

--- argos-service/src/main/java/com/argosnotary/argos/service/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ argos-service/src/main/java/com/argosnotary/argos/service/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -63,7 +63,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         Optional<String> redirectUri = httpCookieOAuth2AuthorizationRequestRepository.getRedirectUri(request);
         URI targetUrl = redirectUri.map(URI::create).orElse(URI.create(getDefaultTargetUrl()));
 
-        String token = tokenProvider.createToken(((ArgosOAuth2User) authentication.getPrincipal()).getAccountId());
+        String token = tokenProvider.createToken(authentication.getPrincipal().getName());
 
         return UriComponentsBuilder.fromUri(frontendRedirectBasePath).path(targetUrl.getPath())
                 .query(targetUrl.getQuery())
